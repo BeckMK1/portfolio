6b6b8a112a3810a1c6fe7b4d8670babe4cf1d04b
Mikkel Beck Knudsen <mikkel1a2@gmail.com>
2020-03-13 13:39:23 +0100

    front page done

---
+++ funtions/firebase.js
@@ -0,0 +1,13 @@
+let firebaseConfig = {
+    apiKey: "AIzaSyCe_79tete6Xe6q8JWBQfe5ox5wtzhNpUo",
+    authDomain: "portfolio-76e4e.firebaseapp.com",
+    databaseURL: "https://portfolio-76e4e.firebaseio.com",
+    projectId: "portfolio-76e4e",
+    storageBucket: "portfolio-76e4e.appspot.com",
+    messagingSenderId: "520012041934",
+    appId: "1:520012041934:web:1c3cc67ba5de56e0eb4a9f",
+    measurementId: "G-JY73FXB72J"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
