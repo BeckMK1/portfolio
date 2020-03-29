a1918ecdc44cd5893cd91b09fd289c4f3b3336c5
Mikkel Beck Knudsen <mikkel1a2@gmail.com>
2020-03-29 17:42:23 +0200

    added call to action buttons

--- js/main.js
+++ js/main.js
@@ -12,50 +12,50 @@ document.querySelector("#moreAni4").addEventListener("mouseover", setActiveAni4)
 function setActiveAni() {
     let arrow1 = document.querySelectorAll("#moreAni1 .arrow1");
 
-    for (let arrow1s of arrow1){
+    for (let arrow1s of arrow1) {
         arrow1s.classList.add("arrow1-ani");
     }
-    let arrow2 =document.querySelectorAll("#moreAni1 .arrow2");
-    for (let arrow2s of arrow2){
+    let arrow2 = document.querySelectorAll("#moreAni1 .arrow2");
+    for (let arrow2s of arrow2) {
         arrow2s.classList.add("arrow2-ani");
     }
-    };
+};
 
-    function setActiveAni2() {
-        let arrow1 = document.querySelectorAll("#moreAni2 .arrow1");
-    
-        for (let arrow1s of arrow1){
-            arrow1s.classList.add("arrow1-ani");
-        }
-        let arrow2 =document.querySelectorAll("#moreAni2 .arrow2");
-        for (let arrow2s of arrow2){
-            arrow2s.classList.add("arrow2-ani");
-        }
-        };
+function setActiveAni2() {
+    let arrow1 = document.querySelectorAll("#moreAni2 .arrow1");
 
-        function setActiveAni3() {
-            let arrow1 = document.querySelectorAll("#moreAni3 .arrow1");
-        
-            for (let arrow1s of arrow1){
-                arrow1s.classList.add("arrow1-ani");
-            }
-            let arrow2 =document.querySelectorAll("#moreAni3 .arrow2");
-            for (let arrow2s of arrow2){
-                arrow2s.classList.add("arrow2-ani");
-            }
-            };
+    for (let arrow1s of arrow1) {
+        arrow1s.classList.add("arrow1-ani");
+    }
+    let arrow2 = document.querySelectorAll("#moreAni2 .arrow2");
+    for (let arrow2s of arrow2) {
+        arrow2s.classList.add("arrow2-ani");
+    }
+};
 
-            function setActiveAni4() {
-                let arrow1 = document.querySelectorAll("#moreAni4 .arrow1");
-            
-                for (let arrow1s of arrow1){
-                    arrow1s.classList.add("arrow1-ani");
-                }
-                let arrow2 =document.querySelectorAll("#moreAni4 .arrow2");
-                for (let arrow2s of arrow2){
-                    arrow2s.classList.add("arrow2-ani");
-                }
-                };
+function setActiveAni3() {
+    let arrow1 = document.querySelectorAll("#moreAni3 .arrow1");
+
+    for (let arrow1s of arrow1) {
+        arrow1s.classList.add("arrow1-ani");
+    }
+    let arrow2 = document.querySelectorAll("#moreAni3 .arrow2");
+    for (let arrow2s of arrow2) {
+        arrow2s.classList.add("arrow2-ani");
+    }
+};
+
+function setActiveAni4() {
+    let arrow1 = document.querySelectorAll("#moreAni4 .arrow1");
+
+    for (let arrow1s of arrow1) {
+        arrow1s.classList.add("arrow1-ani");
+    }
+    let arrow2 = document.querySelectorAll("#moreAni4 .arrow2");
+    for (let arrow2s of arrow2) {
+        arrow2s.classList.add("arrow2-ani");
+    }
+};
 
 // ------------------------ mouse out ------------------------------
 document.querySelector("#moreAni1").addEventListener("mouseout", removeActiveAni)
@@ -66,83 +66,90 @@ document.querySelector("#moreAni4").addEventListener("mouseout", removeActiveAni
 function removeActiveAni() {
     let arrow1 = document.querySelectorAll("#moreAni1 .arrow1");
 
-    for (let arrow1s of arrow1){
+    for (let arrow1s of arrow1) {
         arrow1s.classList.remove("arrow1-ani");
     }
-    let arrow2 =document.querySelectorAll("#moreAni1 .arrow2");
-    for (let arrow2s of arrow2){
+    let arrow2 = document.querySelectorAll("#moreAni1 .arrow2");
+    for (let arrow2s of arrow2) {
         arrow2s.classList.remove("arrow2-ani");
     }
-    };
+};
 
-    function removeActiveAni2() {
-        let arrow1 = document.querySelectorAll("#moreAni2 .arrow1");
-    
-        for (let arrow1s of arrow1){
-            arrow1s.classList.remove("arrow1-ani");
-        }
-        let arrow2 =document.querySelectorAll("#moreAni2 .arrow2");
-        for (let arrow2s of arrow2){
-            arrow2s.classList.remove("arrow2-ani");
-        }
-        };
+function removeActiveAni2() {
+    let arrow1 = document.querySelectorAll("#moreAni2 .arrow1");
 
-        function removeActiveAni3() {
-            let arrow1 = document.querySelectorAll("#moreAni3 .arrow1");
-        
-            for (let arrow1s of arrow1){
-                arrow1s.classList.remove("arrow1-ani");
-            }
-            let arrow2 =document.querySelectorAll("#moreAni3 .arrow2");
-            for (let arrow2s of arrow2){
-                arrow2s.classList.remove("arrow2-ani");
-            }
-            };
+    for (let arrow1s of arrow1) {
+        arrow1s.classList.remove("arrow1-ani");
+    }
+    let arrow2 = document.querySelectorAll("#moreAni2 .arrow2");
+    for (let arrow2s of arrow2) {
+        arrow2s.classList.remove("arrow2-ani");
+    }
+};
 
-            function removeActiveAni4() {
-                let arrow1 = document.querySelectorAll("#moreAni4 .arrow1");
-            
-                for (let arrow1s of arrow1){
-                    arrow1s.classList.remove("arrow1-ani");
-                }
-                let arrow2 =document.querySelectorAll("#moreAni4 .arrow2");
-                for (let arrow2s of arrow2){
-                    arrow2s.classList.remove("arrow2-ani");
-                }
-                };
+function removeActiveAni3() {
+    let arrow1 = document.querySelectorAll("#moreAni3 .arrow1");
+
+    for (let arrow1s of arrow1) {
+        arrow1s.classList.remove("arrow1-ani");
+    }
+    let arrow2 = document.querySelectorAll("#moreAni3 .arrow2");
+    for (let arrow2s of arrow2) {
+        arrow2s.classList.remove("arrow2-ani");
+    }
+};
+
+function removeActiveAni4() {
+    let arrow1 = document.querySelectorAll("#moreAni4 .arrow1");
+
+    for (let arrow1s of arrow1) {
+        arrow1s.classList.remove("arrow1-ani");
+    }
+    let arrow2 = document.querySelectorAll("#moreAni4 .arrow2");
+    for (let arrow2s of arrow2) {
+        arrow2s.classList.remove("arrow2-ani");
+    }
+};
 // -------------------------------- zoom animation --------------------------
 //----------------------------------- mouseover -----------------------------
 document.querySelector("#gaming-match").addEventListener("mouseover", setZoom1)
 document.querySelector("#paly-bet").addEventListener("mouseover", setZoom2)
 document.querySelector("#assens-shipyard").addEventListener("mouseover", setZoom3)
 document.querySelector("#web-doc").addEventListener("mouseover", setZoom4)
-function setZoom1(){
-document.querySelector("#gaming-img").style.width ="72%";
+
+function setZoom1() {
+    document.querySelector("#gaming-img").style.width = "72%";
 }
-function setZoom2(){
-    document.querySelector("#bet-img").style.width ="72%";
-    }
-function setZoom3(){
-    document.querySelector("#shipyard-img").style.width ="72%";
-    }
-function setZoom4(){
-    document.querySelector("#doc-img").style.width ="72%";
-    }
+
+function setZoom2() {
+    document.querySelector("#bet-img").style.width = "72%";
+}
+
+function setZoom3() {
+    document.querySelector("#shipyard-img").style.width = "72%";
+}
+
+function setZoom4() {
+    document.querySelector("#doc-img").style.width = "72%";
+}
 //------------------------------------ mouseout -----------------------------
 document.querySelector("#gaming-match").addEventListener("mouseout", removeZoom1)
 document.querySelector("#paly-bet").addEventListener("mouseout", removeZoom2)
 document.querySelector("#assens-shipyard").addEventListener("mouseout", removeZoom3)
 document.querySelector("#web-doc").addEventListener("mouseout", removeZoom4)
 
-function removeZoom1(){
-document.querySelector("#gaming-img").style.width ="70%";
+function removeZoom1() {
+    document.querySelector("#gaming-img").style.width = "70%";
 }
-function removeZoom2(){
- document.querySelector("#bet-img").style.width ="70%";
+
+function removeZoom2() {
+    document.querySelector("#bet-img").style.width = "70%";
 }
-function removeZoom3(){
-document.querySelector("#shipyard-img").style.width ="70%";
+
+function removeZoom3() {
+    document.querySelector("#shipyard-img").style.width = "70%";
 }
-function removeZoom4(){
-document.querySelector("#doc-img").style.width ="70%";
+
+function removeZoom4() {
+    document.querySelector("#doc-img").style.width = "70%";
 }
